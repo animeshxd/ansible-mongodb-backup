59882544f52c1a752008392e097c206a1bff4fd7
Animesh Murmu <47624427+animeshxd@users.noreply.github.com>
2024-05-27 21:01:50 +0530

    Update init.bash

--- init.bash
+++ init.bash
@@ -1,6 +1,6 @@
 vagrant up
 
-cd playbook/ssl && bash init.sh && cd ../..
+# cd playbook/ssl && bash init.sh && cd ../..
 
 cd playbook && ansible-playbook -i hosts.ini ssl.local.yml && cd ..
 cd playbook && ansible-playbook -i hosts.ini main.yml && cd ..
